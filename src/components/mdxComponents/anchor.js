e105d1ba4604c595c54afff835e2e638f67e7102
Jason Covington <jason_covington@volusion.com>
2020-01-28 16:21:10 -0600

    fix links

--- src/components/mdxComponents/anchor.js
+++ src/components/mdxComponents/anchor.js
@@ -3,7 +3,10 @@ import * as React from 'react'
 const AnchorTag = ({ children: link, ...props }) => {
   if (link) {
     return (
-      <a href={props.href} target="_blank" rel="noopener">
+      <a 
+        href={props.href}
+        // target="_blank"
+        rel="noopener">
         {link}
       </a>
     )
